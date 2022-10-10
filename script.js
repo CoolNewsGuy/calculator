1561d557f49b262bcaad58dae602830cb35748b4
MrLuckyCat <joewilliamdd31@gmail.com>
2022-10-10 23:10:35 +0100

    Add a function to delete symbols in typer

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let operator = document.getElementById("operator"),
-    leftOperand = document.getElementById("left-operand"),
-    rightOperand = document.getElementById("right-operand"),
+"use strict";
+let operation = document.getElementById("operation"),
     result = document.getElementById("result");
 
 function clearTyper() {
-    leftOperand.textContent = 0;
-    operator.textContent = "";
-    rightOperand.textContent = "";
-    result.textContent = "";
+    operation.textContent = "0";
+    result.textContent = "0";
 }
+
+function deleteCharacters() {
+    if (operation.textContent !== "0")
+        operation.innerText = operation.innerText.slice(0, -1);
+    if (operation.textContent === "") operation.innerText = "0";
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") deleteCharacters();
+});
