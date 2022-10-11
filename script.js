959107a196140daa149c148d2ceaad9952eeae59
MrLuckyCat <joewilliamdd31@gmail.com>
2022-10-11 10:23:24 +0100

    Receive user inputs and add shortcuts

--- script.js
+++ script.js
@@ -16,13 +16,48 @@ function deleteCharacters() {
 
 function playShortcuts(e) {
     switch (e.key) {
-        case "Backspace":
-            deleteCharacters();
         case "c":
         case "C":
             clearTyper();
+            break;
+
+        case "Backspace":
+            deleteCharacters();
+            break;
+
+        case "%":
+        case "+":
+            operation.textContent += " " + e.key + " ";
+            break;
+
+        case "÷":
+        case "/":
+            operation.textContent += " " + "÷" + " ";
+            break;
+
+        case "*":
+        case "x":
+        case "X":
+            operation.textContent += " " + "×" + " ";
+            break;
+
+        case "−":
+        case "-":
+            operation.textContent += " " + "−" + " ";
+            break;
+
+        case ".":
+            operation.textContent += ".";
+            break;
     }
 }
 
+function receiveUserInput(e) {
+    if (e.key !== " " && [...Array(10).keys()].includes(+e.key))
+        operation.textContent === "0"
+            ? (operation.textContent = e.key)
+            : (operation.textContent += e.key);
+}
+
 document.addEventListener("keydown", playShortcuts);
-document.addEventListener("keydown", (e) => console.log(e.key));
+document.addEventListener("keypress", receiveUserInput);
